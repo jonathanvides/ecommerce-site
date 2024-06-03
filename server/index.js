2d9409fa05fe2fdb854c98f1b66182ca15c738f1
Jonathan Vides <jonathan@Jonathans-MacBook-Air.local>
2024-06-02 21:07:29 -0500

    Updated README.md

--- server/index.js
+++ server/index.js
@@ -1,2 +1,2 @@
 // Error Handling:
-// Create an error handling middleware to catch an error and throw an error message along with a status
+// Create an error handling middleware to catch an error and throw an error message along with a status.
