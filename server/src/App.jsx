0e96602c7918e806e4f7fae7eec98ebc39852c4c
Jonathan Vides <jonathan@Jonathans-MacBook-Air.local>
2024-06-10 22:15:52 -0500

    Added components and added routers.

---
+++ server/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default App
