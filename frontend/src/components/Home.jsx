5ed8db0503567d228e3db9c45139ddf3382df346
Jonathan Vides <jvides00@hotmail.com>
2024-07-03 21:28:57 -0500

    Updated home page.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect  } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchAllProducts } from '../API/product';
 
