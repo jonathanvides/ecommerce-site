bb09443da05862f75affc652c7791dcca05af94d
Jonathan Vides <jvides00@hotmail.com>
2024-07-04 12:09:31 -0500

    Edited urls for api

--- frontend/src/API/url.js
+++ frontend/src/API/url.js
@@ -1,4 +1,4 @@
 //Easy to use url access to import into files
 
-export const API_URL = 'https://ecommerce-site-backend.onrender.com/api';
-export const BASE_URL = 'https://ecommerce-site-backend.onrender.com';
+export const API_URL = 'https://ecommerce-site-kuaf.onrender.com/api';
+export const BASE_URL = 'https://ecommerce-site-kuaf.onrender.com';
