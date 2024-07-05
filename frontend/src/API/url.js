d761a5faee940fcf1735e6f8515da88cec70e513
Jonathan Vides <jvides00@hotmail.com>
2024-07-05 15:34:22 -0500

    Deployed on netlify

--- frontend/src/API/url.js
+++ frontend/src/API/url.js
@@ -1,4 +1,8 @@
-//Easy to use url access to import into files
+// Easy to use url access to import into files
 
-export const API_URL = 'https://ecommerce-site-kuaf.onrender.com/api';
-export const BASE_URL = 'https://ecommerce-site-kuaf.onrender.com';
+export const API_URL = 'http://localhost:3000/api';
+export const BASE_URL = 'http://localhost:3000';
+
+// Deployed on Render
+// export const API_URL = 'https://ecommerce-site-kuaf.onrender.com/api';
+// export const BASE_URL = 'https://ecommerce-site-kuaf.onrender.com';
